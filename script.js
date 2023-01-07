d982461be274e8badab4612f4217e6d27414b20a
sabanansari <alisaban52@gmail.com>
2023-01-07 15:23:45 +0530

    added laoder

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote(){
+    loading();
     //    Pick a random quote form apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -27,11 +41,13 @@ function newQuote(){
     }
     quoteText.textContent = quote.text;
 
+    complete();
 
 }
 
 // Get Quote from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
 
